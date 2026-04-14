5a1372bae99b02e65cc174170421aff54104344e
SHABBIR_HUSAIN_munna <shabbirrohoman24@gmail.com>
2026-04-14 08:16:13 +0600

    Create Input.tsx

---
+++ src/components/ui/src/components/ui/Input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      {...props}
+      className={`border rounded px-3 py-2 w-full ${props.className || ""}`}
+    />
+  );
+}
